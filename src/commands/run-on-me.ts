a2673965591b1e47290476ddf2aa1685344b425c
WardenDigital <wardendigital@proton.me>
2026-01-27 20:00:47 +0100

    run-on-me

--- src/commands/run-on-me.ts
+++ src/commands/run-on-me.ts
@@ -1,23 +1,51 @@
-import * as vscode from 'vscode';
-import { exec } from 'child_process';
+import * as vscode from "vscode";
+import { exec } from "child_process";
+import { Command } from "../types/command";
 
 export default (context: vscode.ExtensionContext) => {
-      const editor = vscode.window.activeTextEditor;
-      if (!editor) {
-        vscode.window.showErrorMessage('No active file');
+  const editor = vscode.window.activeTextEditor;
+  if (!editor) {
+    vscode.window.showErrorMessage("No active file");
+    return;
+  }
+
+  const filePath = editor.document.fileName;
+
+  const command = context.globalState.get<Array<Command>>("commands");
+
+  if (!command) {
+    vscode.window.showErrorMessage(
+      'RunOnMe is not configured. Find "RunOnMe Settings" in Explorer',
+    );
+    return;
+  }
+
+  const fileName = filePath.split("/").pop() || "file";
+
+  let commandNames = command.map(
+    (cmd) => cmd.name.charAt(0).toUpperCase() + cmd.name.slice(1),
+  );
+
+  const ABORT_OPTION = "Abort";
+
+  vscode.window
+    .showInformationMessage(
+      `Choose an command to run on ${fileName}`,
+      ABORT_OPTION,
+      ...commandNames,
+    )
+    .then((selection) => {
+      if (!selection || selection === ABORT_OPTION) {
         return;
       }
 
-      const filePath = editor.document.fileName;
-
-      const command = context.globalState.get<string>('command');
-
-      if (!command) {
-        vscode.window.showErrorMessage('RunOnMe is not configured. Find "RunOnMe Settings" in Explorer');
+      const cmdToRun = command.find((cmd) => cmd.name === selection);
+      if (!cmdToRun) {
+        vscode.window.showErrorMessage(`Command not found: ${selection}`);
         return;
       }
 
-      const commandToExecute = command.replaceAll("${FILE}", filePath);
+      const commandToExecute = cmdToRun.command.replaceAll("${ME}", filePath);
       vscode.window.showInformationMessage(`executing ${commandToExecute}`);
 
       exec(commandToExecute, (error, stdout, stderr) => {
@@ -26,6 +54,7 @@ export default (context: vscode.ExtensionContext) => {
           return;
         }
 
-        vscode.window.showInformationMessage(stdout || 'Done');
+        vscode.window.showInformationMessage(stdout || "Done");
       });
-    }
+    });
+};
